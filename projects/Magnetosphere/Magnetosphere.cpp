83844de88f22320f5ddd760f885e1e62d550b9ca
Greenyer George <ggreen@lx6-500-37275.ad.helsinki.fi>
2026-09-16 16:18:11 +0300

    fixing parameter add in magnetosphere

--- projects/Magnetosphere/Magnetosphere.cpp
+++ projects/Magnetosphere/Magnetosphere.cpp
@@ -93,7 +93,7 @@ namespace projects {
 
 
       //GG 08.09.26: Adding Resistive shell geometry
-      RP::add<Real>("Shell.shellRadius", "Outer radius of resistive region. Default is 1R_M [m].", P::shellRadius, 2.4397e6);  //1R_M as this is smaller than 1R_E so will default do nothing in Earth case
+      RP::add<Real>("Shell.shellRadius", "Outer radius of resistive region. Default is 1R_M [m].", this->shellRadius, 2.4397e6);  //1R_M as this is smaller than 1R_E so will default do nothing in Earth case
 
 
       // Per-population parameters
